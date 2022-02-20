68133de9287ab0f572d4537613f167923e6c14c3
LGHung <58874175+LGHung@users.noreply.github.com>
2022-02-20 17:43:09 +0700

    first commit

---
+++ create.js
@@ -0,0 +1,42 @@
+
+import {
+    getDatabase,
+    ref,
+    set,
+  } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
+  
+  const db = getDatabase();
+  
+  //Variable DOM
+  var namebox = document.getElementById("Namebox");
+  var rollbox = document.getElementById("Rollbox");
+  var secbox = document.getElementById("Secbox");
+  var email_box = document.getElementById("email_box")
+  var cccd_box = document.getElementById("cccd_box")
+
+  var cash_box = document.getElementById("cash")
+  var cashvnd = 0
+  var cashdollar = 0
+  var insBtn = document.getElementById("Insbtn");
+
+  function insertData() {
+    set(ref(db, "TheStudents/" + rollbox.value), {
+      NameOfStd: namebox.value,
+      RollNo: rollbox.value,
+      Section: secbox.value,
+      email : email_box.value,
+      cccd : cccd_box.value,
+      cashes : cashvnd,
+      cashesdollar : cashdollar
+    })
+      .then(() => {
+        alert("data stored successfully");
+      })
+      .catch((error) => {
+        alert("unsuccessful, error: " + error);
+      });
+  }
+  
+  insBtn.addEventListener("click", insertData);
+  
+  
